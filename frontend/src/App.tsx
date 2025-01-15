dea22dfcfd8c80267c2ab22509ab0aac0826380a
Anase72 <anaseankit@gmail.com>
2025-01-15 23:41:44 +0530

    added barebone tiptap editor

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,17 +2,18 @@ import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import Signup  from './pages/Signup'
 import Signin from './pages/Signin'
 // import { useState } from 'react'
+import TipTap from './components/TipTap'
 
-import './App.css'
+// import './App.css'
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
-  <div className='p-2 bg-black relative'>
+  <div>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<div>helloo</div>} />
+          <Route path='/' element={<TipTap />} />
           <Route path='/signup' element={<Signup />} />
           <Route path='/signin' element={<Signin />} />
           <Route path='/blog' element={<div>helloo</div>} />
