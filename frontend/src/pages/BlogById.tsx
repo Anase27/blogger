23cf4f80d7a9b7312c9bc9776f5b98be0025d398
Anase72 <anaseankit@gmail.com>
2025-02-03 23:59:06 +0530

    added a component to list all the blogs in a page

--- frontend/src/pages/BlogById.tsx
+++ frontend/src/pages/BlogById.tsx
@@ -2,47 +2,26 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { BACKEND_URL } from "./config";
 import axios from "axios";
-import { EditorContent, generateHTML, useEditor } from "@tiptap/react";
-import StarterKit from '@tiptap/starter-kit';
-import Image from "@tiptap/extension-image";
-import Underline from "@tiptap/extension-underline";
 import BlogContent from "./BlogContent";
 
 const BlogById = ()=>{
     const {id} = useParams();
     const [blog,setBlog] = useState();
+
     useEffect(()=>{
         const fetchBlog = async ()=>{
+            // REPLACE WITH YOUR BACKEND URL
             const res = await axios.get(`http://127.0.0.1:8787/api/v1/blog/${id}`);
-            // console.log(res.data);
-            // console.log(()=>generateHTML(res.data,[
-            //         StarterKit,
-            //         Image,
-            //         Underline
-            //     ]));
             setBlog(res.data);
         }
         fetchBlog();
-    },[])
-    
-    // const editor = useEditor({
-    //     shouldRerenderOnTransaction: false,
-    //     editable: false,
-    //     content: generateHTML(blog,[StarterKit,
-    //         Image,
-    //         Underline]),
-    //         extensions: [StarterKit,
-    //             Image,
-    //             Underline],
-    //         })
-    //         console.log(editor);
+    },[id]);
             
     if(!blog) return<>Loading...</>
     return(
         <>
             <div>
-                {/* <EditorContent editor={editor} /> */}
-                <BlogContent content={blog}/>
+                <BlogContent content={blog}/> 
             </div>
         </>
     )
---
+++ frontend/src/pages/BlogList.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import { BACKEND_URL } from "./config";
+import axios from "axios";
+
+
+const BlogList = ()=>{
+
+    const [blogs,setBlogs] = useState();
+
+    useEffect(()=>{
+        const getBlogs = async ()=>{
+            const res = await axios.get(`http://127.0.0.1:8787/api/v1/blog/bulk`);
+            setBlogs(res.data);
+        }
+        getBlogs();
+
+    },[])
+    if(!blogs) return <div>Loading...</div>
+    return(
+        <>
+            <div>
+                <ul>
+                    {blogs.map((blog)=>{
+                        return(
+                            <li key={blog.id}>
+                                <a href={`/${blog.id}`}>
+                                    <div>
+                                        <h2>{blog.title}</h2>
+                                    </div>
+                                </a>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </div>
+        </>
+    )
+}
